c8ba7f9e37c78e29534602d2e3d42143ccd839f0
Fred <Fred@CreativeProjects.Tech>
2017-07-20 10:55:49 +0100

    Fix test image paths

--- opencv/gui/images/gui1.py
+++ opencv/gui/images/gui1.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 # Load an color image in grayscale
-img = cv2.imread('../data/raspberrypi.jpg',0)
+img = cv2.imread('../../data/raspberrypi.jpg',0)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
